7fd56e3bfd042066f7e6fb689e1b61e74303c053
laura <laurakcleve@gmail.com>
2023-12-13 16:26:32 -0700

    add a GET request handler

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -46,5 +49,44 @@ func main() {
 	_, err = db.Exec(add_query)
 	if err != nil {
 		log.Fatalf("Failed to add row: %v", err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT * FROM gamelogs")
+		if err != nil {
+			http.Error(w, "Database query error", http.StatusInternalServerError)
+			return
+		}
+
+		defer rows.Close()
+
+		var logs []GameLog
+		for rows.Next() {
+			var log GameLog
+			if err := rows.Scan(&log.ID, &log.Date, &log.Game, &log.Winner); err != nil {
+				http.Error(w, fmt.Sprintf("Database scan error: %v", err), http.StatusInternalServerError)
+				return
+			}
+			logs = append(logs, log)
+		}
+
+		if err = rows.Err(); err != nil {
+			http.Error(w, fmt.Sprintf("Database rows error: %v", err), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+
+		err = json.NewEncoder(w).Encode(logs)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("JSON encoding error, %v", err), http.StatusInternalServerError)
+		}
+
+	})
+
+	log.Println("Starting server on :8080")
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
 	}
 }
